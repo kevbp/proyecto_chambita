4d7ecf439daebefaf4e7e6669d79f60fdd3b9a52
crios <crios@ADMLAP01SIS02.icyp.com.pe>
2024-12-06 09:51:23 -0500

    SE AGREGO ATRIBUTOS A LA ENTIDAD USUARIO

--- src/main/java/Entidades/Usuario.java
+++ src/main/java/Entidades/Usuario.java
@@ -10,4 +10,29 @@ package Entidades;
  */
 public class Usuario {
     
+    private int idUsuario;
+    private String nombre;
+    private String apellido;
+    private int perfil;
+    private String correo;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getPerfil() {
+        return perfil;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
 }

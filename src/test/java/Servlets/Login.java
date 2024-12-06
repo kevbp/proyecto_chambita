d018bc2571898e1053420ad7baf04287eb74a47a
Kevin Broncano <kevinbroncanop@gmail.com>
2024-12-06 10:58:30 -0500

    Desarollo de interfaz de verificacion

---
+++ src/test/java/Servlets/Login.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package Servlets;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Login {
+    
+    public Login() {
+        
+    }
+    
+    @Test
+    public void testLogin(){
+     Assertions.assertTrue(true);
+    }
+}
